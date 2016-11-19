f7f2fc8f9c77cc12d5c13001c83e632ed5b6dd4a
arid2875 <amariddell@gmail.com>
2016-11-18 22:25:15 -0800

    add dictionary

--- game.rb
+++ game.rb
@@ -2,41 +2,41 @@
 require 'sinatra'
 
 get '/' do
-  message = "hi"     
-#   # create
-#   # @guess = params["guess"].downcase    
-  erb :index  
+      
+  create
+  @guess = params["guess"].downcase    
+  erb :index, :locals => {:guesses = @remaining_guesses} 
 
 end
 
-# @@dictionary = []
+@@dictionary = []
 
-# attr_accessor :remaining_guesses
-# attr_accessor :board
-# attr_accessor :guessed_letters
-# attr_accessor :random_word
+attr_accessor :remaining_guesses
+attr_accessor :board
+attr_accessor :guessed_letters
+attr_accessor :random_word
 
-# helpers do
+helpers do
 
-#   def load_dictionary
-#   	File.readlines('dictionary.txt').each do |line| 
-#   	  if line.chomp.length.between?(5,12)
-#   		  @@dictionary << line.chomp
-#   	  end
-#     end
-#   end  
+  def load_dictionary
+  	File.readlines('dictionary.txt').each do |line| 
+  	  if line.chomp.length.between?(5,12)
+  		  @@dictionary << line.chomp
+  	  end
+    end
+  end  
 
-#   def create(guesses=8)
+  def create(guesses=8)
 
-#     load_dictionary
-#     @random_word = @@dictionary[rand(@@dictionary.length-1)].downcase.split("")
-#     @board = []
-#     @guessed_letters = []
-#     @remaining_guesses = guesses
-#     @random_word.each do
-#   	  @board << "_"
-#     end
-#   end
+    load_dictionary
+    @random_word = @@dictionary[rand(@@dictionary.length-1)].downcase.split("")
+    @board = []
+    @guessed_letters = []
+    @remaining_guesses = guesses
+    @random_word.each do
+  	  @board << "_"
+    end
+  end
 
-# end
+end
 
