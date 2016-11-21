a51f0ffd8011d42c47c4a97b59a6d2519779fb3f
arid2875 <amariddell@gmail.com>
2016-11-20 21:08:13 -0800

    added sessions

--- game.rb
+++ game.rb
@@ -1,12 +1,16 @@
 
 require 'sinatra'
 require_relative 'hangman'
+
+enable :sessions
+set :session_secret, 'hellothere12345'
 get '/' do
       
-  hangman = Hangman.new
+  session[:hangman] = Hangman.new
+  @hangman = session[:hangman]
   guess = params["guess"].to_s.downcase
-  message = hangman.validate_guess(guess)    
-  erb :index, :locals => {:guesses => hangman.remaining_guesses, :board => hangman.display_board, :guessed => hangman.display_guessed_letters, :message => message} 
+  message = @hangman.validate_guess(guess)    
+  erb :index, :locals => {:guesses => @hangman.remaining_guesses, :board => @hangman.display_board, :guessed => @hangman.display_guessed_letters, :message => message} 
   
   # def take_turn(guess)          
   #   display_board
